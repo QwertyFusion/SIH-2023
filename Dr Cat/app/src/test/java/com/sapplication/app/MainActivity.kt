67c5b4a239022e3479affadf3e8f72f417f3f877
Rishi Banerjee <60546350+QwertyFusion@users.noreply.github.com>
2024-01-17 15:50:07 +0530

    Upload codes

---
+++ Dr Cat/app/src/test/java/com/sapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sapplication.app
+
+import com.sapplication.app.appcomponents.base.BaseActivity
+import com.sapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
